a98ed96ccae28d7c598d89f9ab6c296b8a18dc34
Dinesh Nagar <dineshnagarit@gmail.com>
2019-05-08 19:22:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const handleResponse = require('./helpers/responseHandler');
+const CusError = require('./helpers/cusError');
+const responseType = require('./helpers/responseType');
+const bodyParser = require('body-parser');
+
+dotenv.config({ path: './configs/.env' });
+
+mongoose.connect(
+  `mongodb://${process.env.DB_DOMAIN}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    useNewUrlParser: true
+  }
+);
+mongoose.connection.on('error', () => {
+  console.log(`unable to connect to database: mongodb://${process.env.DB_DOMAIN}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+});
+
+
+mongoose.set('useCreateIndex', true);
+mongoose.Promise = Promise;
+
+const app = new express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.options('*', (req, res) => res.status(201).end());
+
+require('./routes')(app);
+
+app.use(function (req, res, next) {
+  let error = new CusError(responseType.NOT_FOUND, 'URL Not Found');
+  logger.log(`warn`, req, res, `Requested URL [${req.url}] not found.`);
+  next(error);
+});
+
+app.use(handleResponse.handleError);
+
+let server = app.listen(process.env.PORT, function () {
+  let port = server.address().port;
+  console.log(`holocontrol service listening at port ${server.address().port}.`);
+});
